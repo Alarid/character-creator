ce5e3e265b4cf9074a14d1c5971ca578f58e045e
Yohann Legrand <yohann.legrand-ext@believe.com>
2022-01-10 19:19:18 +0100

    Use position relative instead of isolation

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -25,6 +25,7 @@ function App() {
 
   return (
     <main className={styles.characterEditor}>
+      <div className={styles.perspectiveBox} />
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
@@ -33,7 +34,6 @@ function App() {
             What sort of adventure will you embark on?{" "}
           </p>
         </header>
-        <div className={styles.perspectiveBox} />
         <div className={styles.controlColumn}>
           <ControlPane
             title="Bodies"
