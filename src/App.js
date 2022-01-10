6012a7a173c7a937383461e58d41ae5ab7302ef2
Yohann Legrand <yohann.legrand-ext@believe.com>
2022-01-10 18:31:48 +0100

    Add perspective bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React from "react"
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor"
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <>
+      <div
+        style={{
+          position: "fixed",
+          backgroundColor: "hsl(195deg, 20%, 86%)",
+          bottom: "0",
+          height: "40vh",
+          width: "100%",
+        }}
+      />
       <CharacterEditor />
       <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
